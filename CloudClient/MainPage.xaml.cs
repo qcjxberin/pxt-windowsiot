fc676aa1fa9fcc9bf253c7b35e721d0fd0fe9f29
Artur Laksberg <arturl@microsoft.com>
2016-04-22 16:38:00 -0700

    Initial checkin

---
+++ CloudClient/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CloudClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.textBlock.Text = "...";
+            work();
+        }
+
+        async Task work()
+        {
+            var serialDeviceSelector = Windows.Devices.SerialCommunication.SerialDevice.GetDeviceSelector();
+
+            // var watcher = Windows.Devices.Enumeration.DeviceInformation.CreateWatcher(serialDeviceSelector, [] as any);
+            var devices = await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync(serialDeviceSelector);
+
+            var count = devices.Count;
+            foreach (var device in devices)
+            {
+                Debug.WriteLine(string.Format("Found device '{0}'", device.Name));
+                foreach (var prop in device.Properties)
+                {
+                    Debug.WriteLine("{0}-{1}", prop.Key, prop.Value);
+                }
+                if (device.Name.Contains("mbed"))
+                {
+                    this.Process(device.Id);
+                }
+            }
+
+            Debug.WriteLine("Here");
+
+            var watcher = Windows.Devices.Enumeration.DeviceInformation.CreateWatcher();
+            watcher.Added += DeviceAdded;
+        }
+
+        private static void DeviceAdded(Windows.Devices.Enumeration.DeviceWatcher sender, Windows.Devices.Enumeration.DeviceInformation args)
+        {
+            Debug.WriteLine("Device added: {0}", args.Id);
+        }
+    }
+}
---
+++ CloudClient/serial.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace CloudClient
+{
+    class DataEntity
+    {
+        public int Light;
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        const int bufferSize = 32;
+
+        enum State
+        {
+            Outside_of_object,
+            Inside_object
+        }
+
+        public async Task Process(string id)
+        {
+            var device = await Windows.Devices.SerialCommunication.SerialDevice.FromIdAsync(id);
+            var currentStr = "";
+            State state = State.Outside_of_object;
+            while (true)
+            {
+                var buffer = new Windows.Storage.Streams.Buffer(bufferSize);
+                try
+                {
+                    var result = await device.InputStream.ReadAsync(buffer, bufferSize, Windows.Storage.Streams.InputStreamOptions.None);
+                    if (result.Length > 0)
+                    {
+                        var str = System.Text.Encoding.ASCII.GetString(result.ToArray());
+                        Debug.WriteLine(string.Format("-->{0}<--", str));
+                        foreach (var c in str)
+                        {
+                            switch (c)
+                            {
+                                case '{':
+                                    switch (state)
+                                    {
+                                        case State.Outside_of_object:
+                                            currentStr = c.ToString();
+                                            state = State.Inside_object;
+                                            break;
+                                        case State.Inside_object:
+                                            // throw new NotSupportedException("Nested JSON valued are not supported");
+                                            // We got into a weird state. Get out.
+                                            currentStr = c.ToString();
+                                            state = State.Inside_object;
+                                            break;
+                                    }
+                                    break;
+                                case '}':
+                                    switch (state)
+                                    {
+                                        case State.Outside_of_object:
+                                            // we started reading mid-stream, but now we're truly outside
+                                            break;
+                                        case State.Inside_object:
+                                            currentStr += c;
+                                            HandleJSONObject(currentStr);
+                                            // Nested are not supported
+                                            state = State.Outside_of_object;
+                                            currentStr = "";
+                                            break;
+                                    }
+                                    break;
+                                default:
+                                    switch (state)
+                                    {
+                                        case State.Outside_of_object:
+                                            // skip this char
+                                            break;
+                                        case State.Inside_object:
+                                            // accumulate it:
+                                            currentStr += c;
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // Most likely, the device just got disconnected. Ignore.
+                }
+            }
+        }
+
+        private void HandleJSONObject(string json)
+        {
+            try
+            {
+                var data = JsonConvert.DeserializeObject<DataEntity>(json);
+                this.textBlock.Text = data.Light.ToString();
+            }
+            catch (JsonException)
+            {
+                this.textBlock.Text = "bad data";
+            }
+
+        }
+    }
+}
